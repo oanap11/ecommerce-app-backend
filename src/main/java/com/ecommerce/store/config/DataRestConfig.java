7ad07fcec5bfe1de3e4873ae73a388354d7e48fb
Oana <oana.apvel@gmail.com>
2024-01-04 19:23:14 +0200

    Expose entity ids

--- src/main/java/com/ecommerce/store/config/DataRestConfig.java
+++ src/main/java/com/ecommerce/store/config/DataRestConfig.java
@@ -2,14 +2,28 @@ package com.ecommerce.store.config;
 
 import com.ecommerce.store.entity.Product;
 import com.ecommerce.store.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -17,6 +31,20 @@ public class DataRestConfig implements RepositoryRestConfigurer {
 
         configureExposure(config, Product.class, unsupportedActions);
         configureExposure(config, ProductCategory.class, unsupportedActions);
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entityTypes = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType entityType : entityTypes){
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 
     private void configureExposure(RepositoryRestConfiguration config, Class<?> entityType, HttpMethod... unsupportedActions) {
